a6e58032258f37c3a5d9279029af0bdce4003cab
Carlos Adrian <Teshynil@users.noreply.github.com>
2023-06-13 20:25:57 -0600

    Fix Wrapper errors monks-active-tiles.js

--- monks-active-tiles.js
+++ monks-active-tiles.js
@@ -1,4 +1,4 @@
-﻿import { registerSettings } from "./settings.js";
+import { registerSettings } from "./settings.js";
 import { WithActiveTileConfig } from "./apps/active-tile-config.js"
 import { ActionConfig } from "./apps/action-config.js";
 import { TileTemplates } from "./apps/tile-templates.js";
@@ -1997,9 +1997,9 @@ export class MonksActiveTiles {
         }
 
         if (game.modules.get("lib-wrapper")?.active) {
-            libWrapper.register("monks-active-tiles", "PlaylistDirectory.prototype._onPlaylistCollapse", playlistCollapse, "MIXED");
+            libWrapper.register("monks-active-tiles", "PlaylistDirectory.prototype._onClickEntryName", playlistCollapse, "MIXED");
         } else {
-            const oldPlaylistCollapse = PlaylistDirectory.prototype._onPlaylistCollapse;
+            const oldPlaylistCollapse = PlaylistDirectory.prototype._onClickEntryName;
             PlaylistDirectory.prototype._onPlaylistCollapse = function (event) {
                 return playlistCollapse.call(this, oldPlaylistCollapse.bind(this), ...arguments);
             }
@@ -2174,16 +2174,16 @@ export class MonksActiveTiles {
         }
 
         if (game.modules.get("lib-wrapper")?.active) {
-            libWrapper.register("monks-active-tiles", "ActorDirectory.prototype._onClickDocumentName", clickDocumentName, "MIXED");
+            libWrapper.register("monks-active-tiles", "ActorDirectory.prototype._onClickEntryName", clickDocumentName, "MIXED");
         } else {
-            const oldClickActorName = ActorDirectory.prototype._onClickDocumentName;
+            const oldClickActorName = ActorDirectory.prototype._onClickEntryName;
             ActorDirectory.prototype._onClickDocumentName = function (event) {
                 return clickDocumentName.call(this, oldClickActorName.bind(this), ...arguments);
             }
         }
 
         if (game.modules.get("lib-wrapper")?.active) {
-            libWrapper.register("monks-active-tiles", "ItemDirectory.prototype._onClickDocumentName", clickDocumentName, "MIXED");
+            libWrapper.register("monks-active-tiles", "ItemDirectory.prototype._onClickEntryName", clickDocumentName, "MIXED");
         } else {
             const oldClickItemName = ItemDirectory.prototype._onClickDocumentName;
             ItemDirectory.prototype._onClickDocumentName = function (event) {
@@ -2192,7 +2192,7 @@ export class MonksActiveTiles {
         }
 
         if (game.modules.get("lib-wrapper")?.active) {
-            libWrapper.register("monks-active-tiles", "JournalDirectory.prototype._onClickDocumentName", clickDocumentName, "MIXED");
+            libWrapper.register("monks-active-tiles", "JournalDirectory.prototype._onClickEntryName", clickDocumentName, "MIXED");
         } else {
             const oldClickJournalName = JournalDirectory.prototype._onClickDocumentName;
             JournalDirectory.prototype._onClickDocumentName = function (event) {
@@ -2201,7 +2201,7 @@ export class MonksActiveTiles {
         }
 
         if (game.modules.get("lib-wrapper")?.active) {
-            libWrapper.register("monks-active-tiles", "SceneDirectory.prototype._onClickDocumentName", clickDocumentName, "MIXED");
+            libWrapper.register("monks-active-tiles", "SceneDirectory.prototype._onClickEntryName", clickDocumentName, "MIXED");
         } else {
             const oldClickJournalName = SceneDirectory.prototype._onClickDocumentName;
             SceneDirectory.prototype._onClickDocumentName = function (event) {
@@ -2210,7 +2210,7 @@ export class MonksActiveTiles {
         }
 
         if (game.modules.get("lib-wrapper")?.active) {
-            libWrapper.register("monks-active-tiles", "MacroDirectory.prototype._onClickDocumentName", clickDocumentName, "MIXED");
+            libWrapper.register("monks-active-tiles", "MacroDirectory.prototype._onClickEntryName", clickDocumentName, "MIXED");
         } else {
             const oldClickJournalName = MacroDirectory.prototype._onClickDocumentName;
             MacroDirectory.prototype._onClickDocumentName = function (event) {
@@ -2226,7 +2226,7 @@ export class MonksActiveTiles {
         }
 
         if (game.modules.get("lib-wrapper")?.active) {
-            libWrapper.register("monks-active-tiles", "SidebarDirectory.prototype._onClickDocumentName", checkClickDocumentName, "MIXED");
+            libWrapper.register("monks-active-tiles", "SidebarDirectory.prototype._onClickEntryName", checkClickDocumentName, "MIXED");
         } else {
             const oldClickJournalName = SidebarDirectory.prototype._onClickDocumentName;
             SidebarDirectory.prototype._onClickDocumentName = function (event) {
@@ -2249,7 +2249,7 @@ export class MonksActiveTiles {
         }
 
         if (game.modules.get("lib-wrapper")?.active) {
-            libWrapper.register("monks-active-tiles", "Compendium.prototype._onClickEntry", clickCompendiumEntry, "MIXED");
+            libWrapper.register("monks-active-tiles", "Compendium.prototype._onClickEntryName", clickCompendiumEntry, "MIXED");
         } else {
             const oldOnClickEntry = Compendium.prototype._onClickEntry;
             Compendium.prototype._onClickEntry = function (event) {
